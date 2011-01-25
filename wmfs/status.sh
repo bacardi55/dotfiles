c59f4d2393a81a6e53ee87703750f0e02e82afd7
Raphael Khaiat <raphael@khaiat.org>
2011-01-25 16:27:15 +0100

    Add new wmfs config

--- wmfs/status.sh
+++ wmfs/status.sh
@@ -1,86 +1,248 @@
 #!/bin/sh
 
-#COLOR
+# AddiKT1ve <the.addikt1ve@gmail.com>
 
-SC="\\#555555\\"
-NC="\\#CCCCCC\\"
-TC="\\#FE0000\\"
-BL="\\#FFFFFF\\"
-NO="\\#000000\\"
-
-calc()
-{
-     echo "`echo $1 | bc -l | cut -d. -f1`"
+_mpd() {
+	############
+	# mpd info
+	#
+	# <mpc> is required for "now playing" informations
+	#
+	if [ "`mpc 2>&1 | wc -l`" -gt "1" ]; then
+		if [ "`mpc | grep "^\[paused\]"`" != "" ]; then
+			mpd_current="`mpc current` [pause]"
+		else
+			mpd_current=`mpc current`
+		fi
+	else
+		mpd_current="\o/"
+	fi
+	mpd="\\#cba642\\$mpd_current"
+	#
+	############
 }
 
-#progressbar x y w h bordcolor bgcolor barcolor value maxvalue
-progressbar()
-{
-    local BARH=`calc "$8/$9*$3"`
-    local BORD=" \b[`calc \"$1 - 1\"`;`calc \"$2 - 1\"`;`calc \"$3 + 2\"`;`calc \"$4 + 2\"`;$5]\ "
-    local BGBAR=" \b[$1;$2;$3;$4;$6]\ "
-    local VBAR=" \b[$1;$2;$BARH;$4;$7]\ "
+_mocp_author(){
+	############
+	# mocp author info
+	#
+	# <mocp> is required for "now playing" informations
+	#
+	author=`mocp -i | grep Artist | awk -F ": " '{print $2}'`
+	mocp_author="\\#cba642\\$author"	
+	#
+	############
 
-    echo "$BORD$BGBAR$VBAR"
 }
 
-# DATE
-DATE=`date +"%H:%M %d/%m/%y"`
-HEURE="$SC |$NC $DATE"
+_mocp_song(){
+	############
+	# mocp author info
+	#
+	# <mocp> is required for "now playing" informations
+	#
+	SONG=`mocp -i | grep SongTitle | awk -F ": " '{print $2}'`
+	#
+	############
+}
 
-#Name 
-USER=`whoami`
-NAME=`uname -n`
-ME="$NC$USER$SC@$NC$NAME$SHL$SC |"
+_network() {
+	############
+	#	network
+	#
+	# network usage stats
+	#
+	# Variables
+	ethiface=eth0
+	wlaniface=wlan0
+	tmpdir=/tmp
+	#
+	# Functions
+	function rx_bytes { # download
+		rxethernet=`cat /sys/class/net/$ethiface/statistics/rx_bytes`
+		rxwireless=`cat /sys/class/net/$wlaniface/statistics/rx_bytes`
+		echo $(($rxethernet+$rxwireless))
+	}
+	#
+	function tx_bytes { # upload
+		txethernet=`cat /sys/class/net/$ethiface/statistics/tx_bytes`
+		txwireless=`cat /sys/class/net/$wlaniface/statistics/tx_bytes`
+		echo $(($txethernet+$txwireless))
+	}
+	#
+	# Download
+	lastrxbytes=`cat $tmpdir/last_rxbytes`
+	# Upload
+	lasttxbytes=`cat $tmpdir/last_txbytes`
+	#
+	# Download
+	rxbytes=`rx_bytes`
+	rxresult=$((($rxbytes-lastrxbytes)/1000))
+	echo $rxbytes > $tmpdir/last_rxbytes
+	#
+	# Upload
+	txbytes=`tx_bytes`
+	txresult=$((($txbytes-lasttxbytes)/1000))
+	echo $txbytes > $tmpdir/last_txbytes
+	#
+	# Output
+	network="\\#81ae51\\↓ $rxresult Ko/s | $txresult Ko/s ↑"
+	#
+	############
+}
 
-#VOLUME BAR
-#SOUND=`amixer -c 0 sset "PCM" 0%+ | grep "Front Left:" | awk -F "%" {'print $1'} | awk -F "[" {'print $2'}`
-#VOLUME="$NC                 $SOUND %"
-#IMG_VOL="\i[1050;1;20;20;/home/bacardi55/.config/wmfs/images/sound.png]\  "
-#VOLBAR=`progressbar 1080 5 55 9 '#FFFFFF' '#000000' '#FE0000' $SOUND 100`
-#VOL1="$VOLBAR$IMG_VOL$VOLUME"
+_battery() {
+	############
+	# battery state
+	#
+	bat_percent=$((`cat /sys/class/power_supply/BAT*/energy_now`/`cat /sys/class/power_supply/BAT*/energy_full | sed 's/00$//'`))
+	bat_acpi=`cat /sys/class/power_supply/BAT*/status`
+	#
+	# use an arrow to show if battery is charging, discharging or full/AC
+	#
+	if [ "$bat_acpi" = "Discharging" ]; then
+		bat_state="↓"
+	elif [ "$bat_acpi" = "Charging" ]; then
+		bat_state="↑"
+	fi
+	#
+	# blinking battery percent indicator if bat_percent < 15
+	#
+	if [ "$bat_percent" -lt "15" ]; then
+		bat_fail=1
+		if [ "`cat /tmp/batteryfail`" ]; then
+			color="\\#ff6b6b\\"
+			echo 0 > /tmp/batteryfail
+		else
+			color="\\#435e87\\"
+			echo 1 > /tmp/batteryfail
+		fi
+	else
+		bat_fail=0
+		color="\\#C0C0C0\\"
+	fi
+	#
+	############
+	# battery time
+	#
+	# <acpi> is required
+	#
+	#bat_remtime="`acpi | cut -d' ' -f5 | cut -d':' -f1,2`"
+	#
+	############
+	battery="$color Bat. $bat_percent% $bat_state"
+}
 
-#CPU BAR
-#CPU=`top -n2 -d0.1 | grep "Cpu" | awk {'print $2'} | cut -d. -f1 | tail -n1`
-#CPUBAR=`progressbar 985 5 55 9 '#FFFFFF' '#000000' '#FE0000' $CPU 100`
-#IMG_CPU="\i[950;1;20;20;/home/bacardi55/.config/wmfs/images/cpu.jpg]\  "
-#CPU="$NC           "
-#CPUF="$CPUBAR$IMG_CPU$CPU"
+_uptime() {
+	############
+	# uptime
+	#
+	uptime=`cut -d'.' -f1 /proc/uptime`
+	secs=$((${uptime}%60))
+	mins=$((${uptime}/60%60))
+	hours=$((${uptime}/3600%24))
+	days=$((${uptime}/86400))
+	uptime="${mins}m ${secs}s"
+	#
+	if [ "${hours}" -ne "0" ]; then
+	  uptime="${hours}h ${uptime}"
+	fi
+	#
+	if [ "${days}" -ne "0" ]; then
+	  uptime="${days}d ${uptime}"
+	fi
+	#
+	############
+}
 
-# Musique
-SONG=`mocp -i | grep SongTitle | awk -F ": " '{print $2}'`
-ARTIST=`mocp -i | grep Artist | awk -F ": " '{print $2}'`
-if [ -z "$ARTIST"]; then
-    MOCP=""
-else
-    MOCP="$NC listenning$TC $SONG$SC ( $NC$ARTIST$SC )"
-fi;
+_memory() {
+	############
+	# memory usage
+	#
+	memory_used="`free -m | sed -n 's|^-.*:[ \t]*\([0-9]*\) .*|\1|gp'`"
+	memory_total="`free -m | sed -n 's|^M.*:[ \t]*\([0-9]*\) .*|\1|gp'`"
+	memory="$memory_used/$memory_total Mo"
+	#
+	############
+}
 
-# Mail
-MAIL=`python /home/bacardi55/.config/wmfs/imap.py | grep UNSEEN | awk {'print $2'}`
-if [ "$MAIL" == '0' ]; then
-    SMAIL="$SC 0 "
-else
-    SMAIL="$TC $MAIL "
-fi;
-IMG_EMAIL="\i[1145;1;20;20;/home/bacardi55/.config/wmfs/images/email.png]\  "
-EMAIL="$IMG_EMAIL$SMAIL"
+_volume() {
+	############
+	# volume
+	#
+	# <amixer> is required
+	#
+	if [ "`amixer get Master | grep '\[off\]$'`" = "" ]; then
+		volume=`amixer get Master | sed -n 's|.*\[\([0-9]*\)\%.*|\1%|pg'`
+	else
+		volume="[off]"
+	fi
+	#
+	############
+}
 
-# prochain rdv : 
-RV=`calcurse -n | grep "\[[0-9]*:[0-9]*\]" | awk -F "] " '{print $2}'`
-RDV="$NC RDV:$TC $RV $SC |"
+_date() {
+	############
+	# date
+	#
+	sys_date=`date '+%a %d %b %Y'`
+	date="\\#ff6b6b\\$sys_date"
+	#
+	############
+}
 
-# HL irssi :
-scp kim:~/.irssi/fnotify ~/.fnotify
-IRSSI=`tail -n1 /home/bacardi55/.fnotify`
-if [ -z "$IRSSI" ]; then
-    HL="    "
-else
-    HL="$TC HL: $IRSSI$SC |    "
-fi;
+_hour() {
+	############
+	# hour
+	#
+	sys_hour=`date '+%H:%M'`
+	hour="\\#1793d1\\$sys_hour"
+	#
+	############
+}
 
-ECRAN0="$ME$HL$EMAIL$HEURE"
-ECRAN1="$ME$HL$RDV$MOCP$HEURE"
+_ompload() {
+	############
+	# ompload
+	#
+	# <ompload> is required
+	# <cropscreen.sh> is required
+	#
+	[ -e /tmp/omploadurl ] && ompload_url=`cat /tmp/omploadurl`
+	ompload="$ompload_url"
+	#
+	############
+}
 
-/usr/local/bin/wmfs -s 0 "$ECRAN0"
-/usr/local/bin/wmfs -s 1 "$ECRAN1"
+_separateur() {
+	separateur="•"
+}
+
+statustext() {
+	############
+	# concatenate arguments
+	#
+	for arg in $@; do
+		_${arg}
+		args="${args}  `eval echo '$'$arg`"
+	done
+	#
+	############
+	# wmfs magic
+	#
+	wmfs -s 0 "$args"
+	#
+	############
+}
+
+############
+# status text
+#
+# add <variables> from the following list
+# mpd network battery uptime volume date hour ompload
+#
+#statustext ompload mpd network battery date hour
+statustext mocp_author separateur battery separateur memory separateur date separateur hour
+#
+############
+
