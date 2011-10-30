f1924de1157a7a366c94ad2b005532b0d460db43
Raphaël <raphael@khaiat.org>
2011-10-30 13:46:08 +0100

    change daily

--- script/dailyTask.sh
+++ script/dailyTask.sh
@@ -2,22 +2,14 @@
 
 WORK="taf" # name of the work tag
 PERSO="perso" #name of the personnal tag
-MAIL="raphael@khaiat.org"
-MAIL_OBJECT="TODO LIST du `date +%d` `date +%B` `date +%G`"
+MAIL_PERSO="raphael@khaiat.org"
+MAIL_PERSO_OBJECT="[PERSO] Todolist du `date +%d` `date +%B` `date +%G`"
+MAIL_WORK="raphael@khaiat.org"
+MAIL_WORK_OBJECT="[WORK] Todolist du `date +%d` `date +%B` `date +%G`"
 
 echo "TODO LIST du `date +%d` `date +%B` `date +%G`
 
-******** PRO ******** "> /tmp/task_intro
-
-echo "
-
-
--------------------------------------
--------------------------------------
--------------------------------------
-
-
-******* PERSO ******* " > /tmp/task_separator
+******** PRO ******** "> /tmp/task_work_intro
 
 # TAF
 echo "TASK(S) FOR TODAY :" > /tmp/task_pre_today_work
@@ -32,6 +24,18 @@ echo "
 
 OPENED TASK(S)" > /tmp/task_pre_work
 task ls +"$WORK" > /tmp/task_work
+
+echo "
+
+AU BOULOT ! :)" > /tmp/task_work_fin
+
+cat /tmp/task_work_intro /tmp/task_pre_today_work /tmp/task_today_work /tmp/task_pre_eow_work /tmp/task_eow_work /tmp/task_pre_work /tmp/task_work /tmp/task_work_fin | mail -s "$MAIL_WORK_OBJECT" "$MAIL_WORK"
+rm /tmp/task*
+
+
+echo "TODO LIST du `date +%d` `date +%B` `date +%G`
+
+******* PERSO ******* " > /tmp/task_perso_intro
 
 # PERSO
 echo "
@@ -55,7 +59,7 @@ task timesheet > /tmp/task_timesheet
 
 echo "
 
-AU BOULOT ! :)" > /tmp/task_fin
+AU BOULOT ! :)" > /tmp/task_perso_fin
 
-cat /tmp/task_intro /tmp/task_pre_today_work /tmp/task_today_work /tmp/task_pre_eow_work /tmp/task_eow_work /tmp/task_pre_work /tmp/task_work /tmp/task_separator /tmp/task_pre_today_perso /tmp/task_today_perso /tmp/task_pre_eow_perso /tmp/task_eow_perso /tmp/task_pre_perso /tmp/task_perso /tmp/task_pre_timesheet /tmp/task_timesheet /tmp/task_fin | mail -s "$MAIL_OBJECT" "$MAIL"
+cat /tmp/task_perso_intro /tmp/task_pre_today_perso /tmp/task_today_perso /tmp/task_pre_eow_perso /tmp/task_eow_perso /tmp/task_pre_perso /tmp/task_perso /tmp/task_pre_timesheet /tmp/task_timesheet /tmp/task_perso_fin | mail -s "$MAIL_PERSO_OBJECT" "$MAIL_PERSO"
 rm /tmp/task*
